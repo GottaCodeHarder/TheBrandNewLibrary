ab57b5549e3b1f6a571c3413b590d1edb16610d0
GottaCodeHarder <masterkonnen@gmail.com>
2015-10-19 17:10:22 +0100

    Apunts Novena Clase!
    Metodes automatics

--- Teoria/[S4-C2] Teoria (const).cpp
+++ Teoria/[S4-C2] Teoria (const).cpp
@@ -1,6 +1,15 @@
-Uso de const, antes de la funcion, en la llamada de argumentos y al final de la funcion. 
- 
+Uso de const, antes de la funcion, en la llamada de argumentos y al final de la funcion.
+
 Al inici(const int) vol dir que no modifica el valor de sortida(return *this), al final vol dir que no modifica el valor d'entrada' (arguments)
 Si esta dentro del argumento(const Point& a) significa que solo estamos leyendolo.
 
 Constructor.Defineix com es construeix una clase.
+
+[S6-C1:Examen]
+En const char* const cadena no es pot modificar res de res, en const char* cadena pots fer cadena++ i cadena = NULL.
+Amb char* const cadena pots fer *cadena = NULL.
+
+const correctness: es quan un programa te tots els const que ha de tenir. Es considera mes robust que un que no te.
+Els const afecten a la execucio de un programa ? En principi NO, encara que deixes que hi hagui certes optimitzacions(entra a master).
+Es mes rapid de desenvolupar, apareixen menys bugs.
+
---
+++ Teoria/[S6-C1] Teoria.cpp
@@ -0,0 +1,64 @@
+====< METODES AUTOMATICS / ESPECIALS >====
+							
+	INVESTIGAR				
+Quan creem una class els compilador creara uns quants metodes encara que tu no els escriguis. Tels crea per tu.
+A vegades son perillosos.	
+							
+class Thing					
+{							
+public: // Tots aquests son els metodes automatics que et crea el compilador.
+	Thing();				// Constructor per defecte. Realment, no fa res, no posa les coses a 0
+	Thing(const Thing&);	// Constructor de Copia, basicament fa una copia: A a1(5); A b1(a1); // classe b1 te el valor de 5 de la a1.
+	~Thing();				// Destructor que es crea automaticament que esta buit i no serveix per res. Per defecte no son virtuals.
+							// Aquests tres sempre es fan
+							
+	// Els que tenen el doble && no conten per l'examen
+	Thing(Thing&&);				
+	/* Aquest es creara si:	-Ø no hi ha el Constructor de Copia (Ø = fet per l'usuari)
+							-Ø si no hi ha l'operador =
+							-Ø si no s'ha fet el de moure
+							-Ø si no s'ha fet el Destructor
+							-si no esta marcat com a delete
+							-si tots els seus membres es poden moure
+	*/
+	Thing operator = (const Thing &&);
+	Thing operator = (Thing &&);
+	/*Aquest es creara si:	-Ø no hi ha el Constructor de Copia
+							-Ø no hi ha l'operador =
+							-Ø no hi ha constructor de moure
+							-Ø no s'ha fet el Destructor
+							-si no esta marcat com a delete
+							-si tots els seus membres es poden moure
+							(lo mateix que amb l'altre)
+	*/						
+};							
+
+// Experiments pel Contructor per Defecte.
+class A
+{
+	int num;
+};
+A* pclass = new A(); // Aixo funciona
+
+class A
+{
+	A(int n) : num(n){}
+	int num;
+};
+A* pclass = new A(); // Aixo ja no funciona. error: "No arguments match the constructors of A" o algo parecido.
+
+
+// Constructor de Copia ejemplo: strings
+string a("hola mundo"); // <== 11 bytes alocatats al heap
+// Esto tiene un char* str; y un uint capacity; <== Aixo son 8 bytes.
+
+string b(a); // fa una copia dels 8 bytes
+// Contiene char* str; uint capacity;
+// Las consequencies son que hi ha 2 clases cadena, pero les dues apunten a la mateixa! <!>
+
+-Probar:
+string a("hola");
+{
+	string b(a);
+}
+a = "adios"; // No peteria perque no has alliberat la memoria
