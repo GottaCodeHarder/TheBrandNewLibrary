c1ff6a56b6ed0201eb55a4b395ff89d600289f8f
GottaCodeHarder <masterkonnen@gmail.com>
2015-11-02 17:18:05 +0100

    [S8-C1] Class String II

--- Teoria/[S7-C1] Class String/[S7-C1] Class String/String.cpp
+++ Teoria/[S7-C1] Class String/[S7-C1] Class String/String.cpp
@@ -4,21 +4,65 @@ using namespace std;
 
 String::String(){}
 
-String::String(const String&)
+String::String(const String& a) // Arreglar
 {
-	this->str = str;
-	this->mem_capacity = mem_capacity;
+	if (&a != NULL)
+	{
+		this->str = str;
+		this->mem_capacity = mem_capacity;
+	}
 }
 
 String::String(const char* a)
 {
-	this->str = a;
+	if (a != NULL)
+	{
+		int size = strlen(a);
 
+		if (size > 0)//In case _str is empty ""
+		{
+			size += 1;
+			str = new char[size];
+			mem_capacity = size;
+			strcpy_s(str, size, a);
+		}
+		else
+		{
+			mem_capacity = 1;
+			str = new char[1];
+			*str = '\0';
+		}
+	}
+	else
+		printf("Error found: The string trying to build is empty");
 }
 
-String::String(const uint a)
+String::String(const uint mem_capacity)
 {
-	this->mem_capacity = a;
+	if (mem_capacity > 0)
+	{
+		str = new char[mem_capacity];
+		this->mem_capacity = mem_capacity;
+	}
+	else printf("Error.");
+}
+
+/*
+bool String::operator== (const String& string) const
+{
+	return strcmp(this->str, string.str) == 0;
+}*/
+
+bool String::operator += (const String& string) // Arreglar
+{
+	bool ret = false;
+	if (string.mem_capacity > 0 && string.str != NULL)
+	{
+		this->mem_capacity = this->mem_capacity + string.mem_capacity;
+		strcat_s(this->str, this->mem_capacity, string.str);
+	}
+	else printf("Error.");
+	return ret;
 }
 
 
